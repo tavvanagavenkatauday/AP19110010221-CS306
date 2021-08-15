043e65dfe880c4590e18d30bd06e4f8a62a0c725
yuvaraj-06 <yuvarajcoder@gmail.com>
2021-08-15 11:15:26 +0530

    added files for week2

--- Week-2/lexi_c.c
+++ Week-2/lexi_c.c
@@ -1,3 +1,4 @@
+  
 #include<stdio.h>
 #include<ctype.h>
 #include<string.h>
@@ -7,52 +8,42 @@ int check_keyword(char s[]) //linear search
 {
 	int i;
 	for(i=0;i<24;++i)
-		if(strcmp(s,keyword[i])==0)//equal means 0 ,compares 1st char ,if s1[0]>s2[1]=>+1 or s1[1]>s2[0]-1
+		if(strcmp(s,keyword[i])==0)
 			return 1;
 	return 0;
 }
- 
-void store_symb_tab(char id[], char symb_tab[][30])//helps to remove duplicates
+
+void store_symb_tab(char id[], char symb_tab[][30])
 {
 	int i;
-	for(i=0; strcmp(symb_tab[i],"")&&i<30;++i)
+	for(i=0; strcmp(symb_tab[i],"")&&i<20;++i)
 		if(!strcmp(symb_tab[i],id))
 			return;
-	
+	if(i==20)
+	{ printf("Overflow!"); return;}// create linked list to avoid this
 	strcpy(symb_tab[i],id); //adds id to symb_tab
 }
 
 int main()
 {
 	FILE *fp1,*fp2;
-	char c,id[20], num[10];
+	char c,id[30], num[10];
 	int state=0,i=0,j=0;
 	fp1=fopen("x.txt","r");//input file containing src prog
-	fp2=fopen("y.txt","w");//output file name
-	//declare symbol table as a double dimensional array of characters.
-	char symb_tab[30][30];
+	fp2=fopen("program.txt","w");//output file name
+	//declare symbol table as a doubly dimensional array of characters.
+	char symb_tab[20][30];
 	
-
-	while((c=fgetc(fp1))!=EOF)//runs until the end of the file
+	while((c=fgetc(fp1))!=EOF)
 	{
 		switch(state)
 		{
-			
-			case 0://"Inital state diverts to correct state"
+			case 0:
                 
-				if(isalpha(c))
-				{ 
-					state=1;
-					id[i++]=c; 
-				}
+				if(isalpha(c)||c=='_')
+				{ state=1; id[i++]=c; }
 				else if(isdigit(c))
-				{
-					state=3;
-
-					num[j]=c;
-					j=j+1; 
-				 
-				}
+				{ state=3; num[j++]=c; }
 				else if(c=='<' || c=='>')
 					state=5;
 				else if(c=='=' || c=='!')
@@ -62,24 +53,18 @@ int main()
 				else if(c==' ' || c=='\t' || c=='\n')
                     
 					state=0;
-				 //checks for newline in file
+				else if(c=='\r'); //checks for newline in file
 				else
 					fprintf(fp2,"\n%c",c);
 				break;
-			case 1://alphabets
-				if(isalnum(c))
-				{
-					state=1; 
-					
-					id[i]=c;
-					i=i+1; }
+			case 1:
+				if(isalnum(c)||c=='_')
+				{ state=1; id[i++]=c; }
 				else{
 					id[i]='\0';
-					
 					if(check_keyword(id))
 						fprintf(fp2," \n%s : keyword ",id);
 					else{
-                        
                         
 						fprintf(fp2,"\n%s : identifier",id);
 						// call a function which stores id in symbol table
@@ -90,25 +75,18 @@ int main()
 					ungetc(c,fp1);
 				}
 				break;
-			case 3://digits
-				 
+			case 3:
 				if(isdigit(c))
-				{ 
-					num[j]=c;
-					j=j+1;
-					state=3;
-				 }
+				{ num[j++]=c; state=3; }
 				else{
-					 
-					num[j]='\0';//when it indetifes that was the ending number it appends null and writes the number to file
-					 
+					num[j]='\0';
 					fprintf(fp2," \n%s: number",num);
 					state=0;
 					j=0;
 					ungetc(c,fp1);
 				}
 				break;
-			case 5://equal and <> in correct order
+			case 5:
 				if(c=='='){
 					//write code to print specific operator like <= or >=
 					fseek(fp1,-2,SEEK_CUR); //go back 2 chars
@@ -152,7 +130,7 @@ int main()
 				}
 				break;
 			case 10:
-				if(c=='*')     
+				if(c=='*')
 					state=11;
 				else{
 					fprintf(fp2,"\n/%c: invalid lexeme",c);
